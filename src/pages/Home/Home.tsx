02196bba55a6442776842785a4d05644c0fd84a9
nagito.terada <nagito.terada@hokuden-it.co.jp>
2023-02-17 23:30:45 +0900

    AppShell やってみた

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,65 @@
 import React from "react";
-import { ActionIcon, Box, Button, Stack, Text, useMantineColorScheme } from "@mantine/core";
+import {
+  ActionIcon,
+  AppShell,
+  Box,
+  Button,
+  Header,
+  Navbar,
+  Stack,
+  Text,
+  Title,
+  useMantineColorScheme,
+} from "@mantine/core";
 import { MdDarkMode, MdLightMode } from "react-icons/md";
+import { FaCompressAlt, FaExpandAlt } from "react-icons/fa";
+
+import { useDebouncedState } from "@mantine/hooks";
 
 export const Home: React.FC = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const dark = colorScheme === "dark";
 
+  const [visibleNav, setVisibleNab] = useDebouncedState(false, 200);
+
   return (
     <Box mt={30}>
-      <Stack align="center">
-        <Text size="xl" weight={500}>
-          Welcome to Mantine!
-        </Text>
-        <Button>Click the button</Button>
+      <AppShell
+        padding="md"
+        hidden={visibleNav}
+        navbar={
+          <Navbar width={{ base: 300 }} height={500} p="xs">
+            <Text>Application sidebar</Text>
+          </Navbar>
+        }
+        header={
+          <Header height={60} p="xs">
+            <Title order={1}>This is h1 title</Title>
+          </Header>
+        }
+        styles={(theme) => ({
+          main: { backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0] },
+        })}
+      >
+        <Stack align="center">
+          <Text size="xl" weight={500}>
+            Welcome to Mantine!
+          </Text>
+          <Button>Click the button</Button>
 
-        <ActionIcon
-          variant="outline"
-          color={dark ? "yellow" : "blue"}
-          onClick={() => toggleColorScheme()}
-          title="Toggle color scheme"
-        >
-          {dark ? <MdLightMode /> : <MdDarkMode />}
-        </ActionIcon>
-      </Stack>
+          <ActionIcon
+            variant="outline"
+            color={dark ? "yellow" : "blue"}
+            onClick={() => toggleColorScheme()}
+            title="Toggle color scheme"
+          >
+            {dark ? <MdLightMode /> : <MdDarkMode />}
+          </ActionIcon>
+          <ActionIcon variant="outline" color={dark ? "yellow" : "blue"} onClick={() => setVisibleNab(!visibleNav)}>
+            {visibleNav ? <FaCompressAlt /> : <FaExpandAlt />}
+          </ActionIcon>
+        </Stack>
+      </AppShell>
     </Box>
   );
 };
